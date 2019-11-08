c86f7c4d7bdff5e24a4bc68983a0c5d546790960
sathya <sathya@sathyas-MacBook-Pro.local>
2019-11-08 18:15:02 -0500

    fixed IE issues

--- src/app/helpers/helpers.ts
+++ src/app/helpers/helpers.ts
@@ -1,4 +1,4 @@
-export const HOST_URL = 'http://localhost:8080/';
+export const HOST_URL = 'http://ec2-3-133-233-94.us-east-2.compute.amazonaws.com/';
 export const ACCESS_TOKEN = 'accessToken';
 
 export function getUrl(uri: string) {
